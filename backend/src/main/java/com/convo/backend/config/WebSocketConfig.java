a51faaf25ed61fffa3cf972aa6c0cef14cf46783
Atika-Tabassum <109681326+Atika-Tabassum@users.noreply.github.com>
2026-04-06 09:25:25 +0600

    frontend url updated

--- backend/src/main/java/com/convo/backend/config/WebSocketConfig.java
+++ backend/src/main/java/com/convo/backend/config/WebSocketConfig.java
@@ -21,7 +21,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(signalingHandler, "/ws")
-                .setAllowedOrigins("http://localhost:5173");
+        registry.addHandler(signalingHandler, "/socket")
+                .setAllowedOrigins("https://convo-frontend-nine.vercel.app/");
     }
 }
