919ed10acdf7b4363b0efc25100d3c19ceb7a5c5
Zaki Rehnoom <zakirehnoom9402@gmail.com>
2026-04-11 12:07:55 +0600

    ci check

--- backend/src/main/java/com/convo/backend/utilities/JSONUtils.java
+++ backend/src/main/java/com/convo/backend/utilities/JSONUtils.java
@@ -1,7 +1,7 @@
 package com.convo.backend.utilities;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import tools.jackson.core.type.TypeReference;
+import tools.jackson.databind.ObjectMapper;
 import java.util.Map;
 
 public class JSONUtils {
